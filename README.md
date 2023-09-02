d3c7129d74390490e15ba43402a99f0893fed713
andrewcs0901 <andrewcs0901@gmail.com>
2023-09-02 16:17:41 -0300

    feat: starting the project

---
+++ README.md
@@ -0,0 +1,1 @@
+# f1-node-rest-db-api
---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port, () => console.log(`Server running on port ${port}`));
